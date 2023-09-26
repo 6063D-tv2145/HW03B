ff46ab1a13195702072cf77feaa5d00b919cb250
Thao Chi Vu <tv2145@nyu.edu>
2023-09-26 16:32:51 -0400

    Added a grid of circles with randomly assigned x & y positions as well as random sizes

--- sketch.js
+++ sketch.js
@@ -1,6 +1,22 @@
+let eDiam = 60;
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(800, 800);
 }
 
 function draw() {
+  background("eggshell");
+  randomSeed(100);
+
+  for (let xpos = 0; xpos < width; xpos += eDiam + random(27, (eDiam/2)+2)) {
+    for (let ypos = 0; ypos < height; ypos += eDiam + random(25, eDiam/2)) {
+      let yindex = random (0,40)
+      let xindex = random (0,40)
+      let circlesize= random(25,eDiam);
+
+        fill(84,142,255)
+        strokeWeight(0)
+        ellipse((xpos + xindex), (ypos + yindex), circlesize);
+    }
+  }
 }
